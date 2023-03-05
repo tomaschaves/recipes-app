5c70cf5bc7b9453b92b91fa419cff3bdd8e44c40
HB <helberbrito@gmail.com>
2023-03-05 16:59:19 -0300

    develop: implementado estrutura do fluxo dos testes para a página "Meals".

---
+++ src/tests/03.meals.main-screen.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Meals from '../Pages/Meals';
+import App from '../App';
+import { renderWithRouterAndProvider } from './helpers/renderWithRouterAndProvider';
+
+afterEach(cleanup);
+
+const VARIAVEIS = 'variaveis';
+
+describe('Teste da página Meals.js', () => {
+  describe('1 - Rota', () => {
+    test.only('1.1 - Se o usuário está na rota "/" ao acessar a aplicação.', () => {
+
+    });
+  });
+
+  describe('2 - Navegação', () => {
+    test('2.1 - Se o usuário é redirecionado para a rota "X" ao realizar determinada ação', () => {
+
+    });
+  });
+
+  describe('3 - Renderização', () => {
+    test('3.1 - Se os elementos ??? estão presentes na tela', () => {
+
+    });
+
+    test('3.2 - Se os elementos ??? estão presentes na tela após determinada ação', () => {
+
+    });
+  });
+
+  describe('4 - Funcionalidade', () => {
+    test.only('4.1 - As regras de negócio em cada cenário possível na interação com o usuário ', () => {
+
+    });
+  });
+});
