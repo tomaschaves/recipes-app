44da2ffa64614155c4a33a2a332714f4aa6b5931
HB <helberbrito@gmail.com>
2023-03-05 16:16:45 -0300

    Implementado função auxiliar "renderWithRouterAndProvier" necessário para testes

---
+++ src/tests/helpers/renderWithRountAndProvider.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import RecipeProvider from '../../context/RecipeProvider';
+
+export function renderWithRouterandContext(
+  component,
+  {
+    initialEntries = ['/'],
+    history = createMemoryHistory({ initialEntries }),
+  } = {},
+) {
+  return {
+    ...render(
+      <RecipeProvider>
+        <Router history={ history }>
+          { component }
+        </Router>
+      </RecipeProvider>,
+    ),
+    history,
+  };
+}
