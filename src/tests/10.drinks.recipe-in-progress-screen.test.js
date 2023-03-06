2f10c81977b31c29b5874b9b671fbab305f29835
HB <helberbrito@gmail.com>
2023-03-05 21:09:16 -0300

    hotfix: conteúdo dos testes "02" ao "10" comentado para não gerar erro no avaliador por causa do linter.

--- src/tests/10.drinks.recipe-in-progress-screen.test.js
+++ src/tests/10.drinks.recipe-in-progress-screen.test.js
@@ -1,40 +1,40 @@
-import React from 'react';
-import { cleanup } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import DrinksInProgress from '../Pages/DrinksInProgress';
-import App from '../App';
-import { renderWithRouterAndProvider } from './helpers/renderWithRouterAndProvider';
+// import React from 'react';
+// import { cleanup } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import DrinksInProgress from '../Pages/DrinksInProgress';
+// import App from '../App';
+// import { renderWithRouterAndProvider } from './helpers/renderWithRouterAndProvider';
 
-afterEach(cleanup);
+// afterEach(cleanup);
 
-const VARIAVEIS = 'variaveis';
+// const VARIAVEIS = 'variaveis';
 
-describe('Teste da página DrinksInProgress.js', () => {
-  describe('1 - Rota', () => {
-    test.only('1.1 - Se o usuário está na rota "/" ao acessar a aplicação.', () => {
+// describe('Teste da página DrinksInProgress.js', () => {
+//   describe('1 - Rota', () => {
+//     test.only('1.1 - Se o usuário está na rota "/" ao acessar a aplicação.', () => {
 
-    });
-  });
+//     });
+//   });
 
-  describe('2 - Navegação', () => {
-    test('2.1 - Se o usuário é redirecionado para a rota "X" ao realizar determinada ação', () => {
+//   describe('2 - Navegação', () => {
+//     test('2.1 - Se o usuário é redirecionado para a rota "X" ao realizar determinada ação', () => {
 
-    });
-  });
+//     });
+//   });
 
-  describe('3 - Renderização', () => {
-    test('3.1 - Se os elementos ??? estão presentes na tela', () => {
+//   describe('3 - Renderização', () => {
+//     test('3.1 - Se os elementos ??? estão presentes na tela', () => {
 
-    });
+//     });
 
-    test('3.2 - Se os elementos ??? estão presentes na tela após determinada ação', () => {
+//     test('3.2 - Se os elementos ??? estão presentes na tela após determinada ação', () => {
 
-    });
-  });
+//     });
+//   });
 
-  describe('4 - Funcionalidade', () => {
-    test('4.1 - As regras de negócio em cada cenário possível na interação com o usuário ', () => {
+//   describe('4 - Funcionalidade', () => {
+//     test('4.1 - As regras de negócio em cada cenário possível na interação com o usuário ', () => {
 
-    });
-  });
-});
+//     });
+//   });
+// });
