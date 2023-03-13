32d5631fe4591a1f53c1d673f39cc41bef241d83
Tomás Chaves <tomcchaves@gmail.com>
2023-03-13 11:25:41 -0300

    Feitos os requisitos 57 a 59 de uma vez. Faremos o resto nessa branch.

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -3,9 +3,19 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function Profile() {
+  const getEmail = () => JSON.parse(localStorage.getItem('user')).email;
+
   return (
     <div>
       <Header title="Profile" />
+      <div>
+        <p data-testid="profile-email">{ getEmail() }</p>
+      </div>
+      <div>
+        <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+        <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
+        <button type="button" data-testid="profile-logout-btn">Logout</button>
+      </div>
       <Footer />
     </div>
   );
