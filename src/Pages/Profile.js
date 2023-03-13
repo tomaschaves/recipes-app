91d95f6545ae7b84ec4f04fb113ddac38107c7f7
Tomás Chaves <tomcchaves@gmail.com>
2023-03-13 11:33:17 -0300

    Requisitos 60 a 62.

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function Profile() {
+  const history = useHistory();
   const getEmail = () => JSON.parse(localStorage.getItem('user')).email;
+
+  const mrStarkIDontFeelSoGood = () => {
+    localStorage.clear();
+    history.push('/');
+  };
 
   return (
     <div>
@@ -12,9 +19,30 @@ export default function Profile() {
         <p data-testid="profile-email">{ getEmail() }</p>
       </div>
       <div>
-        <button type="button" data-testid="profile-done-btn">Done Recipes</button>
-        <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
-        <button type="button" data-testid="profile-logout-btn">Logout</button>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          Done Recipes
+
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          Favorite Recipes
+
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ mrStarkIDontFeelSoGood }
+        >
+          Logout
+
+        </button>
       </div>
       <Footer />
     </div>
