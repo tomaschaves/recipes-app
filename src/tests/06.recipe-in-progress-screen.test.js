a21f72419eb09a8e3f1bd1c4a6ce542a79a140a0
HB <helberbrito@gmail.com>
2023-03-20 07:33:21 -0300

    refat/RecipeInProgess

--- src/tests/06.recipe-in-progress-screen.test.js
+++ src/tests/06.recipe-in-progress-screen.test.js
@@ -1,3 +1,29 @@
-test('', () => {
+import userEvent from '@testing-library/user-event';
+import { renderWithRouterAndProvider } from './helpers/renderWithRouterAndProvider';
+import RecipeInProgress from '../Pages/RecipeInProgress';
 
+describe('01 - MEALS: Teste o componente <Meals/>', () => {
+  test.only('Favoritar: ao clicar no icone de coração, a receita é favoritada e o icone de coração preenchido é renderizado.', async () => {
+    // DAAAM | DEFINIR | ACESSAR | AGIR | AFERIR | MOCKAR
+    const { findByTestId } = renderWithRouterAndProvider(<RecipeInProgress />, { initialEntries: ['/meals/52997'] });
+    // ACESSAR
+    const favoriteButton = await findByTestId('favorite-btn');
+    // AGIR
+    userEvent.click(favoriteButton);
+    // AFERIR
+    expect(favoriteButton.alt).toBe('favorito');
+  });
+});
+
+describe('02 - DRINKS: Teste o componente <Drinks/>', () => {
+  test('Favoritar: ao clicar no icone de coração, a receita é favoritada e o icone de coração preenchido é renderizado.', async () => {
+    // DAAAM | DEFINIR | ACESSAR | AGIR | AFERIR | MOCKAR
+    const { findByTestId } = renderWithRouterAndProvider(<RecipeInProgress />, { initialEntries: ['/drinks/15997'] });
+    // ACESSAR
+    const favoriteButton = await findByTestId('favorite-btn');
+    // AGIR
+    userEvent.click(favoriteButton);
+    // AFERIR
+    expect(favoriteButton.alt).toBe('favorito');
+  });
 });
