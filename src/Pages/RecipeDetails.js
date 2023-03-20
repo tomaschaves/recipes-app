155d7d110f912509e1ad1776dbbe75a0307afe1f
HB <helberbrito@gmail.com>
2023-03-20 05:31:05 -0300

    develop/test: 100% cobertura em RecipeDetails. hotfix: refatorado RecipeDetails

--- src/Pages/RecipeDetails.js
+++ src/Pages/RecipeDetails.js
@@ -4,38 +4,63 @@ import MealDetails from '../components/MealDetails';
 import DrinkDetails from '../components/DrinkDetails';
 
 export default function RecipeDetails() {
-  const location = useLocation();
+  const { pathname } = useLocation();
   const [shownRecipe, setShowRecipe] = useState([]);
 
-  // decidimos qual fetch utilizar com base no pathname do location. setamos o resultado no estado
+  // decidimos qual fetch utilizar com base no pathname do  setamos o resultado no estado
+  // const rightFetch = useCallback(() => {
+  //   console.log(pathname);
+  //   if (/meals/.test(pathname)) {
+  //     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${(pathname.replace(/\D/g, ''))}`)
+  //       .then((response) => response.json())
+  //       .then((data) => setShowRecipe([data.meals[0]]));
+  //   } else if (/drinks/.test(pathname)) {
+  //     fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${(pathname.replace(/\D/g, ''))}`)
+  //       .then((response) => response.json())
+  //       .then((data) => setShowRecipe([data.drinks[0]]));
+  //   }
+  // }, [pathname]);
+
+  // REQUISIÇÃO - na API com endpoint especifico da receita a detalhar
   const rightFetch = useCallback(() => {
-    if (/meals/.test(location.pathname)) {
-      fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${(location.pathname.replace(/\D/g, ''))}`)
+    // VALIDAÇÃO - da condição que define qual endpoint utilizar
+    const mealsRecipe = /meals/.test(pathname);
+    // ACESSA - o id da receita
+    const meal = pathname.replace(/\D/g, '');
+    const drink = pathname.replace(/\D/g, '');
+    // DEFINE - o endpoint com o id da receita
+    const endPointMeals = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal}`;
+    const endPointDrinks = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drink}`;
+
+    // AFERE - a condição para definir qual endpoint utilizar na requisição à API
+    if (mealsRecipe) {
+      fetch(endPointMeals)
         .then((response) => response.json())
         .then((data) => setShowRecipe([data.meals[0]]));
-    } else if (/drinks/.test(location.pathname)) {
-      fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${(location.pathname.replace(/\D/g, ''))}`)
-        .then((response) => response.json())
-        .then((data) => setShowRecipe([data.drinks[0]]));
+      return;
     }
-  }, [location.pathname]);
+
+    fetch(endPointDrinks)
+      .then((response) => response.json())
+      .then((data) => setShowRecipe([data.drinks[0]]));
+  }, []);
 
   // usamos para fazer um array com todos os ingredientes que retornam como chaves do objeto (strIngredient1, strIngredient2, ...)
   const getAllIngredients = () => {
     let index = 1;
     const ingredients = [];
-    if (shownRecipe.length < 1) {
-      return [];
-    }
+    // if (shownRecipe.length < 1) {
+    //   return [];
+    // }
     // enquanto não chega à strIngredient20, o código abaixo roda, colocando os ingredientes no array, para fazermos o map deles futuramente
     while (shownRecipe[0][`strIngredient${index}`] !== ''
     && shownRecipe[0][`strIngredient${index}`] !== null) {
       ingredients.push(`${shownRecipe[0][`strIngredient${index}`]} 
       - ${shownRecipe[0][`strMeasure${index}`]}`);
-      const limit = 19;
-      if (index > limit) {
-        break;
-      }
+      // const limit = 19;
+      // if (index > limit) {
+      //   break;
+      // }
       index += 1;
     }
     return ingredients;
@@ -50,7 +75,7 @@ export default function RecipeDetails() {
   // console.log(ingredientsFunction());
   // renderizamos o componente com base no pathname atual
   const renderComponent = () => {
-    if (/meals/.test(location.pathname)) {
+    if (/meals/.test(pathname)) {
       return (
         <MealDetails
         // passamos a receita atual como props, além do array de ingredientes para .map
